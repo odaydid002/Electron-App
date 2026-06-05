c90645708894dc298c4d4c52a6c15c08517b102f
Oudai Oulhadj <odaydid002@gmail.com>
2026-06-05 15:30:47 +0100

    feat: add Electron app launch and teardown for Playwright tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,4 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, _electron } from '@playwright/test';
+
+let electronApp: Awaited<ReturnType<typeof _electron.launch>>;
+let mainPage: Awaited<ReturnType<typeof electronApp.firstWindow>>;
+
+test.beforeEach(async () => {
+  electronApp = await _electron.launch({
+    args: ['.'],
+    env: {NODE_ENV: 'development'},
+  });
+  mainPage = await electronApp.firstWindow();
+});
+
+test.afterEach(async () => {
+  await electronApp.close();
+})
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
